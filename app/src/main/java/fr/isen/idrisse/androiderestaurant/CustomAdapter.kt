d2698eb8586857866f81265bdc8c54c4ff227108
IdrisseBoss <idrisseaa93@gmail.com>
2023-01-31 10:59:34 +0100

    second morning

--- app/src/main/java/fr/isen/idrisse/androiderestaurant/CustomAdapter.kt
+++ app/src/main/java/fr/isen/idrisse/androiderestaurant/CustomAdapter.kt
@@ -1,21 +1,27 @@
 package fr.isen.idrisse.androiderestaurant
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import fr.isen.idrisse.androiderestaurant.databinding.CellCustomBinding
 
-class CustomAdapter: RecyclerView.Adapter<CustomAdapter.CellViewHolder>() {
-    class CellViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView)
+class CustomAdapter(listOf: List<String>) : RecyclerView.Adapter<CustomAdapter.CellViewHolder>() {
+    class CellViewHolder (binding: CellCustomBinding) : RecyclerView.ViewHolder(binding.root) {
+        val textView:TextView = binding.textView
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CellViewHolder {
-        TODO("Not yet implemented")
+        val binding = CellCustomBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return CellViewHolder(binding)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return 10
     }
 
     override fun onBindViewHolder(holder: CellViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.textView.text = "item "+position
     }
 }
--- app/src/main/java/fr/isen/idrisse/androiderestaurant/MenuActivity.kt
+++ app/src/main/java/fr/isen/idrisse/androiderestaurant/MenuActivity.kt
@@ -3,18 +3,30 @@ package fr.isen.idrisse.androiderestaurant
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
 import fr.isen.idrisse.androiderestaurant.databinding.ActivityMenuBinding
 
+
+enum class Category {ENTREE, PLAT, DESSERT}
 class MenuActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMenuBinding
+
+    companion object {
+        val extraKey = "extraKey"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding =  ActivityMenuBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val category = intent.getSerializableExtra(extraKey) as? Category
 
+        supportActionBar?.title = categoryName(category ?: Category.ENTREE)
+        showDatas()
+
+/*
         val buttonID = intent.getIntExtra("buttonID", 0)
 
         if (buttonID == 0)
@@ -29,8 +41,21 @@ class MenuActivity : AppCompatActivity() {
         {
             binding.textViewMenu.text = getString(R.string.menuDesserts)
         }
+*/
+    }
 
+    private fun showDatas(){
+        binding.recyclerView.layoutManager= LinearLayoutManager(this)
+        binding.recyclerView.adapter= CustomAdapter(listOf("1", "2", "3"))
+    }
 
+    private fun categoryName (category: Category):String{
+        return when(category)
+        {
+            Category.ENTREE -> "ENTREES"
+            Category.PLAT -> "PLATS"
+            Category.DESSERT -> "DESSERTS"
+        }
     }
 
     override fun onStart () {
