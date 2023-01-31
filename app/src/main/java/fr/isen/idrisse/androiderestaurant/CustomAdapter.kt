6bf4b154efc15f050578d77cb9b428daaf515c13
IdrisseBoss <idrisseaa93@gmail.com>
2023-01-31 09:54:21 +0100

    first morning

---
+++ app/src/main/java/fr/isen/idrisse/androiderestaurant/CustomAdapter.kt
@@ -0,0 +1,21 @@
+package fr.isen.idrisse.androiderestaurant
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class CustomAdapter: RecyclerView.Adapter<CustomAdapter.CellViewHolder>() {
+    class CellViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CellViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: CellViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
